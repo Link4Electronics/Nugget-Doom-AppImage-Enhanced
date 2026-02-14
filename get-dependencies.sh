f375612845b24ec33c482a7ad7b04946a9d6ebab
Link4Electronics <link4electronics@gmail.com>
2026-02-14 12:46:30 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,18 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    cmake      \
+    fluidsynth \
+    sdl2       \
+    sdl2_net
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package nugget-doom
 
 # If the application needs to be manually built that has to be done down here
 
