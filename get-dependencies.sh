9680163c2a7baecf374b2f19c24ddcc9a55445da
Link4Electronics <link4electronics@gmail.com>
2026-08-23 19:43:51 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,14 +7,14 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    cmake          \
-    fluidsynth     \
-    sdl2           \
+    cmake      \
+    fluidsynth \
+    sdl2       \
     sdl2_net
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
 # Comment this out if you need an AUR package
 make-aur-package nugget-doom
